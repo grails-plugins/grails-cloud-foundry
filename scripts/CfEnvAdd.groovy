620c3f75333b99eb56bc44350a06569986d7af8a
Burt Beckwith <burt@burtbeckwith.com>
2011-04-17 17:58:51 -0400

    added support for viewing, adding and removing env vars, and new cf-frameworks and cf-runtimes scripts

---
+++ scripts/CfEnvAdd.groovy
@@ -0,0 +1,51 @@
+/* Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.vmware.appcloud.client.CloudApplication
+import com.vmware.appcloud.client.CloudApplication.AppState
+
+/**
+ * @author Burt Beckwith
+ */
+
+includeTargets << new File("$cloudFoundryPluginDir/scripts/CfRestart.groovy")
+
+USAGE = '''
+grails cf-env-add <variable> <value> [--appname]
+'''
+
+target(cfEnvAdd: 'Add an environment variable to an application') {
+	depends cfInit
+
+	doWithTryCatch {
+
+		String name = getRequiredArg(0)
+		String value = getRequiredArg(1)
+
+		CloudApplication application = getApplication()
+		Map<String, String> env = application.env()
+
+		env[name] = value
+		print "Adding Environment Variable [$name=$value]: "
+		client.updateApplicationEnv application.name, env
+		println 'OK'
+
+		if (application.state == AppState.STARTED) {
+			cfRestart()
+		}
+	}
+}
+
+setDefaultTarget cfEnvAdd
---
+++ scripts/CfEnvDel.groovy
@@ -0,0 +1,52 @@
+/* Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.vmware.appcloud.client.CloudApplication
+import com.vmware.appcloud.client.CloudApplication.AppState
+
+/**
+ * @author Burt Beckwith
+ */
+
+includeTargets << new File("$cloudFoundryPluginDir/scripts/CfRestart.groovy")
+
+USAGE = '''
+grails cf-env-del <variable> [--appname]
+'''
+
+target(cfEnvDel: 'Delete an environment variable from an application') {
+	depends cfInit
+
+	doWithTryCatch {
+
+		String name = getRequiredArg()
+
+		CloudApplication application = getApplication()
+		Map<String, String> env = application.env()
+
+		String old = env.remove(name)
+		print "\nDeleting Environment Variable [$name]: "
+		if (old) {
+			client.updateApplicationEnv application.name, env
+		}
+		println 'OK\n'
+
+		if (application.state == AppState.STARTED) {
+			cfRestart()
+		}
+	}
+}
+
+setDefaultTarget cfEnvDel
