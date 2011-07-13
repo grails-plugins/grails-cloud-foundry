3754f13e3f753ad8daf27a403843199ed5a8252c
Burt Beckwith <burt@burtbeckwith.com>
2011-07-13 14:22:36 -0400

    1.0 release

--- CloudFoundryGrailsPlugin.groovy
+++ CloudFoundryGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-/* Copyright 2011 the original author or authors.
+/* Copyright 2011 SpringSource.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,7 @@ import grails.plugin.cloudfoundry.MongoServiceInfo
 
 class CloudFoundryGrailsPlugin {
 
-	String version = '1.0.0.M3'
+	String version = '1.0'
 	String grailsVersion = '1.3.3 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'beckwithb@vmware.com'
