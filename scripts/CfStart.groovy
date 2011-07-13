fe832953415cd3ec4d29b4c65a3149dd3c48d82a
Burt Beckwith <burt@burtbeckwith.com>
2011-07-13 01:46:45 -0400

    GPCLOUDFOUNDRY-3 displaying all associated urls when starting

--- scripts/CfStart.groovy
+++ scripts/CfStart.groovy
@@ -87,7 +87,15 @@ target(cfStart: 'Start an application') {
 			}
 		}
 
-		println "\nApplication '$application.name' started.\n"
+		def urls = new StringBuilder()
+		String delimiter = ''
+		for (String uri in application.uris) {
+			urls.append delimiter
+			urls.append 'http://'
+			urls.append uri
+			delimiter = ', '
+		}
+		println "\nApplication '$application.name' started at $urls\n"
 	}
 }
 
