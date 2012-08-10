5cabfb7f0f3271fad58890222560d63b28862241
Burt Beckwith <beckwithb@vmware.com>
2012-08-10 01:59:56 -0400

    GPCLOUDFOUNDRY-43

---
+++ scripts/CfTunnelDisconnect.groovy
@@ -0,0 +1,48 @@
+/* Copyright 2012 SpringSource.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Burt Beckwith
+ */
+
+includeTargets << new File("$cloudFoundryPluginDir/scripts/_CfCommon.groovy")
+
+USAGE = '''
+grails cf-tunnel-disconnect
+'''
+
+target(cfTunnelDisconnect: 'Disconnects the active tunnel in interactive mode') {
+	depends cfInit
+
+	doWithTryCatch {
+
+		Caldecott = classLoader.loadClass('grails.plugin.cloudfoundry.Caldecott')
+
+		if (!Caldecott.instance) {
+			error 'No active tunnel found'
+			return
+		}
+
+		disconnect()
+	}
+}
+
+disconnect = { ->
+	println "Closing Caldecott tunnel ..."
+	Caldecott.instance?.close()
+	println "Closed tunnel"
+}
+
+setDefaultTarget cfTunnelDisconnect
