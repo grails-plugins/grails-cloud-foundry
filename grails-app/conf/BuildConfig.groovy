45040e40db75c86c553e0a0a4239aaeea7962284
Burt Beckwith <beckwithb@vmware.com>
2012-08-09 23:20:20 -0400

    update to release plugin 2.0.4

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -33,7 +33,7 @@ grails.project.dependency.resolution = {
 	plugins {
 		compile ':cloud-support:1.0.11'
 
-		build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+		build(':release:2.0.4', ':rest-client-builder:1.0.2') {
 			export = false
 		}
 	}
